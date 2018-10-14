061005c680f7401991cf0fbd33eb5c52762b9329
induodinum <korpong555king@gmail.com>
2018-10-14 12:28:29 +0700

    Added SmartwatchData folder

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -27,13 +27,16 @@ def calculate_si_m(data_arr):
 
     return std_axis
 
-filename = ["Accelerometer-2011-04-11-13-28-18-brush_teeth-f1.txt",
-            "Accelerometer-2011-05-30-10-34-16-brush_teeth-m1.txt",
-            "Accelerometer-2011-05-30-21-55-04-brush_teeth-m2.txt",
-            "Accelerometer-2011-03-24-09-51-07-walk-f1.txt",
-            "Accelerometer-2011-05-30-21-44-35-walk-m2_1.txt",
-            "Accelerometer-2012-05-30-19-09-54-walk-m4.txt",
-            "Accelerometer-2011-03-24-10-04-32-pour_water-f1.txt"]
+filename = ["Dataset/Accelerometer-2011-04-11-13-28-18-brush_teeth-f1.txt",
+            "Dataset/Accelerometer-2011-05-30-10-34-16-brush_teeth-m1.txt",
+            "Dataset/Accelerometer-2011-05-30-21-55-04-brush_teeth-m2.txt",
+            "Dataset/Accelerometer-2011-03-24-09-51-07-walk-f1.txt",
+            "Dataset/Accelerometer-2011-05-30-21-44-35-walk-m2_1.txt",
+            "Dataset/Accelerometer-2012-05-30-19-09-54-walk-m4.txt",
+            "Dataset/Accelerometer-2011-03-24-10-04-32-pour_water-f1.txt",
+            "Dataset/Accelerometer-2011-03-24-10-24-39-climb_stairs-f1.txt",
+            "Dataset/Accelerometer-2011-05-30-10-20-47-climb_stairs-m1.txt",
+            "Dataset/Accelerometer-2011-05-30-21-36-58-climb_stairs-m2.txt"]
 std_arr = []
 si_sq_arr = []
 
@@ -74,7 +77,7 @@ for f in filename:
     print("=======================")
 
     g = 9.8  # Gravitational constant
-    window_size = 40
+    window_size = 10
 
     count = 0
     idx = 0
@@ -93,15 +96,9 @@ for f in filename:
             si_m = calculate_si_m(data_arr)
             for i in range(3):
                 sum_std_sq += si_m[i]
-                # print("Si_m: ",si_m[i])
-                # print(" ")
             
             current_si_sq = si_sq_arr[file_count]
             diff_std = (sum_std_sq - current_si_sq)/current_si_sq
-        
-            # print(diff_std/3)
-            # print(max(diff_std/3,0))
-            # print("=======================")
 
             activity_idx = math.sqrt(max(diff_std/3,0))
             # print("Activity Index: ", activity_idx)
@@ -126,6 +123,18 @@ for f in filename:
     print(" ")
 
     ai_all_act.append(ai_arr)
+
+    # Write all AI to a text file
+    filename_ai = "Dataset/AI_" + file.name.split("/")[1]
+    afile = open(filename_ai,"w")
+    count = 0       # A file contains only 15 AIs.
+    for a_val in ai_arr:
+        if(count==15):
+            break
+        afile.write(str(a_val) + "\n")
+        count += 1
+
+
 
 np_all_act = np.array(ai_all_act)
 # print("All activity's AI: ", ai_all_act)
