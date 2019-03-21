63c3540cd3254cb618126cef74e1ffc32be866b0
Korpong Sawataksornchuen <korpong555king@gmail.com>
2019-03-21 14:59:49 +0700

    fix walk algo backup#1

---
+++ src/database/amd_database.sql
@@ -0,0 +1,40 @@
+create table Patient(
+	ID int not null, 
+	Dateandtime datetime not null,
+	X float(6,4), 
+	Y float(6,4), 
+	Z float(6,4), 
+	HR float(6,4),
+    primary key(ID, Dateandtime)
+    );
+                    
+create table Predicted(
+	ID int not null,
+	Dateandtime datetime not null,
+	X float(6,4),
+	Y float(6,4), 
+	Z float(6,4), 
+	Label varchar(255),
+    foreign key(ID, Dateandtime) references Patient(ID, Dateandtime)
+    );
+                    
+create table AllDaySummary(
+	ID int not null,
+    Dateandtime datetime not null,
+    DurationSit time,
+    DurationSleep time,
+    DurationStand time,
+    DurationWalk time,
+    foreign key(ID, Dateandtime) references Patient(ID, Dateandtime)
+    );
+    
+create table ActivityAnalysis(
+	ID int not null,
+    Dateandtime datetime not null,
+    CountActive int,
+    CountInactive int,
+	CountTransition int,
+    CountTotalActiveness int,
+    DurationPerTransition time,
+    foreign key(ID, Dateandtime) references Patient(ID, Dateandtime)
+);
