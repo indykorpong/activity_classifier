22413fe0be50c69192c454d939401368e8899e6e
induodinum <korpong555king@gmail.com>
2019-03-24 21:07:27 +0700

    backup mysql connector

--- src/database/amd_database.sql
+++ src/database/amd_database.sql
@@ -1,31 +1,36 @@
 create table Patient(
 	ID int not null, 
-	Dateandtime datetime not null,
+	DateAndTime datetime not null,
+    Milliseconds varchar(255) not null,
 	X float(6,4), 
 	Y float(6,4), 
 	Z float(6,4), 
-	HR float(6,4),
+	HR float(7,4),
     Label varchar(255),
-    primary key(ID, Dateandtime)
+    primary key(ID, Dateandtime, Milliseconds)
     );
-                    
+
+ 
 create table AllDaySummary(
 	ID int not null,
-    Dateandtime datetime not null,
+    DateTimeFrom datetime not null,
+    DateTimeUntil datetime not null,
     DurationSit time,
     DurationSleep time,
     DurationStand time,
     DurationWalk time,
-    foreign key(ID, Dateandtime) references Patient(ID, Dateandtime)
+    foreign key(ID) references Patient(ID)
     );
     
 create table ActivityAnalysis(
 	ID int not null,
-    Dateandtime datetime not null,
+    DateTimeFrom datetime not null,
+    DateTimeUntil datetime not null,
+    Milliseconds int not null,
     CountActive int,
     CountInactive int,
 	CountTransition int,
     CountTotalActiveness int,
     DurationPerTransition time,
-    foreign key(ID, Dateandtime) references Patient(ID, Dateandtime)
+    foreign key(ID) references Patient(ID)
 );
