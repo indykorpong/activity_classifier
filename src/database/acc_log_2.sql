a1af0f9373e480b4a4f5614a2cac807311c1d923
induodinum <korpong555king@gmail.com>
2019-05-01 16:43:40 +0700

    completed process flow

--- src/database/acc_log_2.sql
+++ src/database/acc_log_2.sql
@@ -1,5 +1,9 @@
 use cu_amd;
 
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+SET GLOBAL MAX_EXECUTION_TIME=1000;
+
 create table acc_log_2(
 	event_timestamp datetime(3) not null,
      x decimal(20,8),
@@ -14,44 +18,26 @@ SELECT * FROM acc_log_2;
 
 delete from acc_log_2;
 
-select distinct event_timestamp, x, y, z, user_id from cu_amd.accelerometer_log as table1;
-
 insert into acc_log_2(event_timestamp, x, y, z, user_id) 
 select event_timestamp, x, y, z, user_id from
 (select distinct event_timestamp, x, y, z, user_id from cu_amd.accelerometer_log) as table1;
 
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-
-select ID, event_timestamp, count(event_timestamp) from cu_amd.acc_log_2
-group by event_timestamp having count(event_timestamp) > 1;
-
-SET GLOBAL MAX_EXECUTION_TIME=1000;
-
-delete t1 from cu_amd.acc_log_2 t1, cu_amd.acc_log_2 t2
-where t1.ID < t2.ID and t1.event_timestamp = t2.event_timestamp;
-
-insert into acc_log_2 (event_timestamp, x, y, z, user_id)
-select distinct event_timestamp, x, y, z, user_id from accelerometer_log
-where event_timestamp > 
-(SELECT max(event_timestamp) as max_timestamp 
-FROM acc_log_2);
-
 SELECT * FROM cu_amd.accelerometer_log;
 
-SELECT * FROM acc_log_2;
-update cu_amd.acc_log_2 set loaded_flag=NULL;
-
 delete from ActivityLog
-where DateAndTime > date_format('2019-04-30 16:48:56.787', '%Y-%m-%d %H:%i:%S.%f') 
+where DateAndTime > date_format('2019-05-01 00:00:00.000', '%Y-%m-%d %H:%i:%S.%f') 
 or UserID = 18;
 
 delete from acc_log_2
-where event_timestamp > date_format('2019-04-30 16:48:56.787', '%Y-%m-%d %H:%i:%S.%f')
+where event_timestamp > date_format('2019-05-01 00:00:00.000', '%Y-%m-%d %H:%i:%S.%f')
+or user_id = 18;
+
+delete from hr_log_2
+where event_timestamp > date_format('2019-05-01 00:00:00.000', '%Y-%m-%d %H:%i:%S.%f')
 or user_id = 18;
 
 delete from ActivityPeriod
-where Date = date_format('2019-04-30', '%Y-%m-%d')
-and ActualFrom >= time_format('16:48:56.787', '%H:%i:%S.%f')
+where Date >= date_format('2019-05-01', '%Y-%m-%d')
 or UserID = 18;
 
 insert into cu_amd.acc_log_2 (event_timestamp, x, y, z, user_id)
@@ -64,11 +50,5 @@ SELECT `AUTO_INCREMENT`
 FROM  INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = 'cu_amd'
 AND TABLE_NAME = 'accelerometer_log';
-
-create table MaxTimestamp(
-	UserID int not null,
-	MaxTimestamp datetime(3) not null,
-    primary key(UserID, MaxTimestamp)
-);
 
 delete from acc_log_2;
--- src/database/python_files/main.py
+++ src/database/python_files/main.py
@@ -18,7 +18,6 @@ import time
 from datetime import datetime, timedelta
 
 path_to_module = '/var/www/html/python/mysql_connect/python_files'
-# path_to_module = '/Users/Indy/Desktop/coding/Dementia_proj/src/database/python_files'
 
 sys.path.append(path_to_module)
 os.chdir(path_to_module)
@@ -26,7 +25,6 @@ os.chdir(path_to_module)
 # # Set data path
 
 basepath = '/var/www/html/python/mysql_connect/'
-# basepath = '/Users/Indy/Desktop/coding/Dementia_proj/src/database/python_files'
     
 datapath = basepath + 'DDC_Data/'
 mypath = basepath + 'DDC_Data/raw/'
@@ -35,13 +33,13 @@ from predict.predict import predict_label
 from summarize.summarize import get_summary
 from load_data.load_data import load_raw_data
 
-from insert_db.insert_db import connect_to_database, clear_table, reset_status, get_distinct_user_ids
+from insert_db.insert_db import connect_to_database, create_temp_table, reset_status, get_distinct_user_ids
 from insert_db.insert_db import update_summarized_flag, get_unpredicted_data, get_unsummarized_data
 from insert_db.insert_db import insert_db_act_period, insert_db_hourly_summary, insert_db_act_log, insert_db_status
 
-# # All Day Data
 datetime_format = "%Y-%m-%d %H:%M:%S.%f"
 
+# Get the current timestamp
 def time_str_now():
     return datetime.now().strftime(datetime_format)[:-3]
 
@@ -50,24 +48,31 @@ status_stopped = 1
 status_error = -1
 
 def get_all_day_result(all_patients):
-    # Load data
-    
+    # Load data, predict activities, and get summary for each patient
+
     for user_id in all_patients:
+
+        #### Load and preprocess data
+
+        # Get the reset status of all process status (load, predict, summarize status respectively)
         all_status = reset_status()
         all_status[0] = status_started
-        start_time = time_str_now()
-        print(start_time)
 
+        start_time = time_str_now()
+
+        # Insert process status into AuditLog table in database
         # StartTime, EndTime, UserID, ProcessName, StartingData, EndingData, ProcessStatus
         insert_db_status(start_time, None, user_id, 'LOAD DATA', None, None, all_status[0])
         # try:
+
+        # Load raw data from accelerometer_log table in database and preprocess the data
         df_all_p = load_raw_data(user_id)
 
         print("loaded data of user: {}".format(user_id))
         print('all status:', all_status)
-        print('head')
-        print(df_all_p.head())
 
+        # If the preprocessed data is not empty for the patient, insert them into
+        # ActivityLog table in database
         if(not df_all_p.empty):
             insert_db_act_log(df_all_p)
 
@@ -78,10 +83,10 @@ def get_all_day_result(all_patients):
 
         stop_time = time_str_now()
 
+        # If there are no new data from smartwatch, insert error status into AuditLog table in database
         if(df_all_p.empty and all_status[0]!=status_error):
             print('no new data')
             all_status[0] = status_error
-            
             insert_db_status(start_time, stop_time, user_id, 'LOAD DATA', None, \
                 None, all_status[0])
 
@@ -91,94 +96,102 @@ def get_all_day_result(all_patients):
             insert_db_status(start_time, stop_time, user_id, 'LOAD DATA', df_all_p.loc[0, 'timestamp'], \
                 df_all_p.loc[df_all_p.shape[0]-1, 'timestamp'], all_status[0])
 
-        print(all_status)
+        print('all status:', all_status)
             
-        
-        # Predict
+        #### Predict
 
+        # Get the unpredicted data from ActivityLog table in database
         df_to_predict = get_unpredicted_data(user_id)
+        df_to_predict = df_to_predict.reset_index(drop=True)
         
-        loop_length = df_all_p.shape[0]
-        chunk_length = loop_length
+        all_status = reset_status()
+        print('reset status:', all_status)
+        
+        # If the data is smaller than the time window length, then skip
+        window_length = 60
+        if(df_to_predict.shape[0]<window_length):
+            continue
 
-        for i in range(0, loop_length, chunk_length):
-            all_status = reset_status()
-            print('reset status:', all_status)
+        all_status[1] = status_started
+        start_time = time_str_now()
+        insert_db_status(start_time, None, user_id, 'PREDICT', None, None, all_status[1])
 
-            df_chunk = df_to_predict[i:i+chunk_length]
-            df_chunk = df_chunk.reset_index(drop=True)
-            
-            window_length = 60
-            if(df_chunk.shape[0]<window_length):
-                continue
+        # try:
+        print("started predicting on user: {}".format(user_id))
+        
+        # Predict activity labels of the patient
+        df_all_p_sorted = predict_label(df_to_predict)
 
-            all_status[1] = status_started
-            start_time = time_str_now()
-            insert_db_status(start_time, None, user_id, 'PREDICT', None, None, all_status[1])
-            # try:
-            print("started predicting")
-            
-            df_all_p_sorted = predict_label(df_chunk)
-            insert_db_act_log(df_all_p_sorted, update=True)
+        # Update the Label field in ActivityLog table in database
+        insert_db_act_log(df_all_p_sorted, update=True)
 
-            print("finished predicting")
+        print("finished predicting on user: {}".format(user_id))
 
-            all_status[1] = status_stopped
-            
-            # except:
-            #     all_status[1] = status_error
+        all_status[1] = status_stopped
+        
+        # except:
+        #     all_status[1] = status_error
 
-            stop_time = time_str_now()
-            print(all_status, start_time, stop_time)
-            insert_db_status(start_time, stop_time, user_id, 'PREDICT', df_all_p_sorted.loc[0, 'timestamp'], \
-                df_all_p_sorted.loc[df_all_p_sorted.shape[0]-1, 'timestamp'], all_status[1])
+        stop_time = time_str_now()
+        print(all_status, start_time, stop_time)
+        insert_db_status(start_time, stop_time, user_id, 'PREDICT', df_all_p_sorted.loc[0, 'timestamp'], \
+            df_all_p_sorted.loc[df_all_p_sorted.shape[0]-1, 'timestamp'], all_status[1])
 
-            # # Analyze Predicted Results
+        #### Summarize
 
-            all_status[2] = status_started
-            start_time = time_str_now()
-            insert_db_status(start_time, None, user_id, 'SUMMARIZE RESULTS', None, None, all_status[2])
+        all_status[2] = status_started
+        start_time = time_str_now()
+        insert_db_status(start_time, None, user_id, 'SUMMARIZE RESULTS', None, None, all_status[2])
 
-            df_to_summarize = get_unsummarized_data(user_id)
-            print('df to summarize')
-            print(df_to_summarize.head())
-            print()
-            print(df_to_summarize.tail())
+        # Get the unsummarized data from ActivityLog table in database
+        df_to_summarize = get_unsummarized_data(user_id)
 
-            # try:
-            df_summary_all, df_act_period = get_summary(df_to_summarize)
+        # try:
+        print("started summarizing on user: {}".format(user_id))
 
-            print('finished summarizing')
-            insert_db_hourly_summary(df_summary_all)
-            insert_db_act_period(df_act_period)
+        # Summarize the predicted results and get activity summary and activity period for the patient
+        df_summary_all, df_act_period = get_summary(df_to_summarize)
 
-            all_status[2] = status_stopped
-            update_summarized_flag(user_id)
+        # Insert the summary and activity period into HourlyActivitySummary table 
+        # and ActivityPeriod table in database respectively
+        insert_db_hourly_summary(df_summary_all)
+        insert_db_act_period(df_act_period)
 
-            # except:
-            #     all_status[2] = status_error
-            
-            stop_time = time_str_now()
-            insert_db_status(start_time, stop_time, user_id, 'SUMMARIZE RESULTS', df_all_p_sorted.loc[0, 'timestamp'], \
-                df_all_p_sorted.loc[df_all_p_sorted.shape[0]-1, 'timestamp'], all_status[2])
+        print('finished summarizing on user: {}'.format(user_id))
 
-            print('all status', all_status)
+        all_status[2] = status_stopped
+
+        # Set the SummarizedFlag in ActivityLog table in database to TRUE(1)
+        update_summarized_flag(user_id)
+
+        # except:
+        #     all_status[2] = status_error
+        
+        stop_time = time_str_now()
+        insert_db_status(start_time, stop_time, user_id, 'SUMMARIZE RESULTS', df_all_p_sorted.loc[0, 'timestamp'], \
+            df_all_p_sorted.loc[df_all_p_sorted.shape[0]-1, 'timestamp'], all_status[2])
+
+        print('all status:', all_status)
 
 def main_function():
     all_patients = get_distinct_user_ids()
-    print(all_patients)
+    print('all patients:', all_patients)
 
-    clear_table()     # delete every row in some tables
+    create_temp_table()     # Import data from original tables to temporary tables
 
     get_all_day_result(all_patients)
 
 if(__name__=='__main__'):
-    # schedule_time = "19:17"
+    schedule_time = "00:00"
+
+    # Schedule the program to run main function every XXXX
+    schedule.every().minute.do(main_function)
+    
     # schedule.every().day.at(schedule_time).do(main_function)
 
-    # while 1:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-    #     print('waiting')
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+        print('waiting')
 
-    main_function()
+    # main_function()
